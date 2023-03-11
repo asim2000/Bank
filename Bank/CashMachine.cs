ac04b76e7b67a570683aa60a47681178edcf879f
Asim Babayev <Asim Babayev@DESKTOP-D4M20DL>
2023-03-11 14:27:10 +0400

    Add project files.

---
+++ Bank/CashMachine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    internal class CashMachine
+    {
+        public static int BankNote(int amount)
+        {
+            if (amount >= 200) return 200;
+            else if (amount >= 100) return 100;
+            else if (amount >= 50) return 50;
+            else if (amount >= 20) return 20;
+            else if (amount >= 10) return 10;
+            else if (amount >= 5) return 5;
+            else return 1;
+        }
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter amount");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            while (amount > 0)
+            {
+                int result = CashMachine.BankNote(amount);
+                Console.WriteLine(result);
+                amount -= result;
+            }
+        }
+    }
+}
